dee5f260a91152b306c553344003a14169be6432
Crawron <azertyaaron@gmail.com>
2021-08-07 21:17:19 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,120 @@
+import React, { useState, useEffect } from "react"
+import history from "history/browser"
+
+import {
+	calcAverages,
+	calcDeltaProbs,
+	calcResultingProbs,
+	filterRolls,
+	formatPercentage,
+	getNumberParam,
+	getRollsFromQuery,
+	range,
+} from "./helpers"
+
+function App() {
+	const [diceCount, setDiceCount] = useState(getNumberParam("count") ?? 2)
+	const [minFace, setMinFace] = useState(getNumberParam("min") ?? 1)
+	const [maxFace, setMaxFace] = useState(getNumberParam("max") ?? 6)
+
+	const [rollCounts, setRollCounts] = useState(getRollsFromQuery())
+
+	useEffect(() => {
+		const { origin, pathname } = window.location
+
+		const params = new URLSearchParams()
+		if (diceCount !== 2) params.set("count", diceCount.toString())
+		if (minFace !== 1) params.set("min", minFace.toString())
+		if (maxFace !== 6) params.set("max", maxFace.toString())
+
+		const rolls = Object.entries(filterRolls(rollCounts, minRoll, maxRoll))
+		for (const [face, count] of rolls)
+			if (count > 0) params.set(face, count.toString())
+
+		const paramsString = params.toString()
+		const url = `${origin}${pathname}?${paramsString}`
+
+		history.replace(url)
+	})
+
+	const minRoll = diceCount * minFace
+	const maxRoll = diceCount * maxFace
+
+	const averageProbs = calcAverages(diceCount, minFace, maxFace)
+
+	const resultingProbs = calcResultingProbs(
+		filterRolls(rollCounts, minRoll, maxRoll)
+	)
+	const deltaProbs = calcDeltaProbs(resultingProbs, averageProbs)
+
+	return (
+		<div className="App">
+			<div className="flex flex-col">
+				<label>
+					Dice Count
+					<input
+						className="text-blueGray-800"
+						type="number"
+						min="1"
+						max="5"
+						name="dice-count"
+						value={diceCount}
+						onChange={(ev) => setDiceCount(parseInt(ev.target.value))}
+					/>
+				</label>
+				<label>
+					Min Dice Value
+					<input
+						className="text-blueGray-800"
+						type="number"
+						min="0"
+						max={maxFace}
+						name="dice-count"
+						value={minFace}
+						onChange={(ev) => setMinFace(parseInt(ev.target.value))}
+					/>
+				</label>
+				<label>
+					Max Dice Value
+					<input
+						className="text-blueGray-800"
+						type="number"
+						min={minFace}
+						name="dice-count"
+						value={maxFace}
+						onChange={(ev) => setMaxFace(parseInt(ev.target.value))}
+					/>
+				</label>
+			</div>
+			<input
+				type="button"
+				value="Clear"
+				className="text-blueGray-800"
+				onClick={() => setRollCounts({})}
+			/>
+			{range(minRoll, maxRoll).map((face) => (
+				<label className="tabular-nums flex flex-row gap-2" key={face}>
+					{face}{" "}
+					<input
+						className="text-blueGray-800 w-10"
+						type="number"
+						value={rollCounts[face] ?? 0}
+						min="0"
+						onChange={(ev) =>
+							setRollCounts({
+								...rollCounts,
+								[face]: parseInt(ev.target.value),
+							})
+						}
+					/>
+					{formatPercentage(deltaProbs[face])} (
+					{formatPercentage(resultingProbs[face] ?? 0)}
+					{" / "}
+					{formatPercentage(averageProbs[face] ?? 0, false)})
+				</label>
+			))}
+		</div>
+	)
+}
+
+export default App
---
+++ src/helpers.ts
@@ -0,0 +1,101 @@
+export function range(min: number, max: number) {
+	const array = []
+	for (let i = min; i <= max; i++) array.push(i)
+	return array
+}
+
+export function calcAverages(
+	diceCount: number,
+	minValue: number = 1,
+	maxValue: number = 6
+): Record<number, number> {
+	const rollCounts: Record<number, number> = {}
+
+	const dice = new Array<number>(diceCount).fill(minValue)
+	const faceCount = maxValue - minValue + 1
+
+	const sum = (arr: number[]) => arr.reduce((p, c) => p + c, 0)
+	let roll = sum(dice)
+
+	const totalPermutations = Math.pow(faceCount, diceCount)
+
+	for (let i = 0; i < totalPermutations; i++) {
+		rollCounts[roll] = (rollCounts[roll] ?? 0) + 1
+
+		// Increment
+		for (let i = 0; i < dice.length; i++) {
+			dice[i] = ((dice[i] - minValue + 1) % faceCount) + minValue
+			if (dice[i] !== minValue) break
+		}
+
+		roll = sum(dice)
+	}
+
+	for (const [roll, count] of Object.entries(rollCounts)) {
+		rollCounts[Number(roll)] = count / totalPermutations
+	}
+
+	return rollCounts
+}
+
+export function formatPercentage(num: number, includeSign = true) {
+	num *= 100
+	const sign = lit(includeSign, getSign(num))
+	return `${sign}${Math.abs(num).toFixed(2)}%`
+}
+
+export function getSign(num: number) {
+	return num > 0 ? "+" : num < 0 ? "-" : "±"
+}
+
+export function lit(condition: boolean, str: string) {
+	return condition ? str : ""
+}
+
+export function calcResultingProbs(rolls: Record<number, number>) {
+	const sum = Object.entries(rolls)
+		.map(([k, v]) => [parseInt(k), v])
+		.reduce((s, [_, c]) => s + c, 0)
+
+	if (sum === 0) return {}
+	return Object.fromEntries(Object.entries(rolls).map(([f, c]) => [f, c / sum]))
+}
+
+export function filterRolls(
+	rolls: Record<number, number>,
+	min: number,
+	max: number
+): Record<number, number> {
+	return Object.fromEntries(
+		Object.entries(rolls)
+			.map<[number, number]>(([k, v]) => [parseInt(k), v])
+			.filter(([f]) => f >= min && f <= max)
+	)
+}
+
+export function calcDeltaProbs(
+	results: Record<number, number>,
+	average: Record<number, number>
+) {
+	return Object.fromEntries(
+		Object.entries(average).map(([k, v]) => [k, (results[Number(k)] ?? 0) - v])
+	)
+}
+
+export function getRollsFromQuery(): Record<number, number> {
+	/* https://stackoverflow.com/a/901144 */
+	const urlSearchParams = new URLSearchParams(window.location.search)
+	return Object.fromEntries(
+		[...urlSearchParams.entries()]
+			.map(([k, v]) => [parseInt(k), parseInt(v)] as [number, number])
+			.filter(([k, v]) => isFinite(k) && isFinite(v))
+	)
+}
+
+export function getNumberParam(key: string) {
+	const urlSearchParams = new URLSearchParams(window.location.search)
+	const value = parseInt(urlSearchParams.get(key) ?? "")
+
+	if (!isFinite(value)) return undefined
+	else return value
+}
